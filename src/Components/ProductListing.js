f0d076826def994981492d00b2f2438f43348a69
Pooja4183 <poojatomar.jt@gmail.com>
2026-04-03 20:26:20 +0530

    product dashboard UI

--- src/Components/ProductListing.js
+++ src/Components/ProductListing.js
@@ -4,8 +4,8 @@ import ProductCard from "./ProductCard";
 const ProductListing = ({ products = [], loading }) => {
   return (
     <>
-      <div className="px-10 py-12">
-        <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+      <div className="px-14 ">
+        <div className="flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
           {products.length > 0 ? (
             products.map((item) => (
               <ProductCard key={item._id} item={item} />
